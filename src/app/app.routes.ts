c413c07569f788d805b4394c3c18785e69aa2c60
David-Samir-Luis <Davidsamir07@gmail.com>
2026-03-01 20:52:41 +0200

    signIn, register and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/register/register.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/login/login.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
+import { ForgetPasswordComponent } from './features/forget-password/forget-password.component';
+import { FeedComponent } from './features/feed/feed.component';
+import { ProfileComponent } from './features/profile/profile.component';
+import { NotificationComponent } from './features/notification/notification.component';
+import { ChangePasswordComponent } from './features/change-password/change-password.component';
+import { hasTokenGuard } from './core/auth/guards/has-token-guard';
+import { isLoginedGuard } from './core/auth/guards/is-logined-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"" ,redirectTo:"login", pathMatch:"full"},
+    {path:"" ,component:AuthLayoutComponent ,canActivate:[isLoginedGuard]
+        ,children:[
+        {path:"login" ,component:LoginComponent , title:"login page"},
+        {path:"register", component:RegisterComponent , title:"register page"},
+        {path:"forgetPassword", component:ForgetPasswordComponent , title:"forget password page"}
+
+    ]},
+
+    {path:"" ,component:MainLayoutComponent ,canActivate:[hasTokenGuard],
+        children:[
+        {path:"feed" ,component:FeedComponent , title:"feed page"},
+        {path:"profile" ,component:ProfileComponent , title:"profile page"},
+        {path:"notification",component:NotificationComponent , title:"notification page"},
+        {path:"changePassword",component:ChangePasswordComponent , title:"change password page"}
+    ]},
+    {path: "**" ,component:NotFoundComponent , title:"not found page"}
+];
